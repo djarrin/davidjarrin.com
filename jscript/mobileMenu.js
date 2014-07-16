d74e1fce69624c90199189ada60428259ce3b169
djarrin <dmjarrin@gmail.com>
2014-07-15 22:16:41 -0400

    changes page once the page in the drop down menu is selected. The current page shows as the default value in the drop down menu

--- jscript/mobileMenu.js
+++ jscript/mobileMenu.js
@@ -4,12 +4,25 @@ $(document).ready(function () {
     var $option;
 
     $('nav ul a').each(function () {
+        var $anchor = $(this);
+
         $option = $('<option></option>');
+
+        if ($anchor.parent().hasClass("selected")) {
+            $option.prop("selected", true);
+        }
+
         $option.val($(this).attr('href'));
         $option.text($(this).text());
         $select.append($option);
+
     });
 
     $('nav').append($select);
+
+    //will change to the selected page
+    $select.change(function () {
+        window.location = $select.val();
+    });
 
 });

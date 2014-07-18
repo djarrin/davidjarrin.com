4beee21de92a99f404035912d6b83f625eebcf7c
djarrin <dmjarrin@gmail.com>
2014-07-18 17:47:54 -0400

    gives three bar navigation button but doesn't have any of the previous styling's

--- jscript/mobileMenu.js
+++ jscript/mobileMenu.js
@@ -1,28 +1,11 @@
-$(document).ready(function () {
+$("#nav").addClass("js").before('<div id="menu">&#9776</div>');;
 
-    var $select = $('<select></select>');
-    var $option;
+$("#menu").click(function(){
+	$("#nav").slideToggle();
+});
 
-    $('nav ul a').each(function () {
-        var $anchor = $(this);
-
-        $option = $('<option></option>');
-
-        if ($anchor.parent().hasClass("selected")) {
-            $option.prop("selected", true);
-        }
-
-        $option.val($(this).attr('href'));
-        $option.text($(this).text());
-        $select.append($option);
-
-    });
-
-    $('nav').append($select);
-
-    //will change to the selected page
-    $select.change(function () {
-        window.location = $select.val();
-    });
-
+$(window).resize(function(){
+	if(window.innerWidth > 768) {
+		$("#nav").removeAttr("style");
+	}
 });

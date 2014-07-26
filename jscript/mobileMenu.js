b785969e670ec99c24a97ea15148d223a33b0eb1
djarrin <dmjarrin@gmail.com>
2014-07-26 11:09:53 -0400

    fixed mobile header and #nav spacing, still have box shadow bug

--- jscript/mobileMenu.js
+++ jscript/mobileMenu.js
@@ -1,13 +1,13 @@
 $("#nav").addClass("js").before('<div id="menu">&#9776</div>');
 $("#nav").addClass("js").before('<div id="mobileHeader">David Jarrin</div>');
 
-$("#menu").click(function () {
+$("#menu").click(function () {  //will toggle nav menu if 
     $("#nav").slideToggle();
 });
 
 $(window).resize(function () {
     if (window.innerWidth > 768) {
         $("#nav").removeAttr("style");
-      
+        
     }
 });

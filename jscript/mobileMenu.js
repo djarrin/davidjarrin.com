a6e70578f6056e647e4999b63f67592b38033512
djarrin <dmjarrin@gmail.com>
2014-07-31 20:55:10 -0400

    have set up selected tabs, eliminated pics until they are ready, mobileMenu needs to be debugged to handle box-shadows

--- jscript/mobileMenu.js
+++ jscript/mobileMenu.js
@@ -1,9 +1,26 @@
 $("#nav").addClass("js").before('<div id="menu">&#9776</div>');
 $("#nav").addClass("js").before('<div id="mobileHeader">David Jarrin</div>');
 
+
+
 $("#menu").click(function () {  //will toggle nav menu if 
+    var toggleState = 0;
+    function toggleMenu() {
+        if (toggleState == 0) {
+            toggleState = 1;
+            $('#menu').removeClass(shadowBox);
+            $('#nav').createClass(shadowBox);
+        } else {
+            toggleState = 0;
+            $('#nav').removeClass(shadowBox);
+            $('#menu').createClass(shadowBox);
+        }
+     }
     $("#nav").slideToggle();
 });
+
+
+
 
 $(window).resize(function () {
     if (window.innerWidth > 768) {
